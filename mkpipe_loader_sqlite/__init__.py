455b6bda7689979e42b0b9b8847754fce9cc0a5f
m-karakus <s.metinkarakus@gmail.com>
2026-03-02 23:53:49 +0300

    new structue

--- mkpipe_loader_sqlite/__init__.py
+++ mkpipe_loader_sqlite/__init__.py
@@ -1,16 +1,13 @@
 import os
-from mkpipe.functions_spark import BaseLoader
+
+from mkpipe.spark import JdbcLoader
 
 
-class SqliteLoader(BaseLoader):
-    def __init__(self, config, settings):
-        super().__init__(
-            config,
-            settings,
-            driver_name='sqlite',
-            driver_jdbc='org.sqlite.JDBC',
-        )
+class SqliteLoader(JdbcLoader, variant='sqlite'):
+    driver_name = 'sqlite'
+    driver_jdbc = 'org.sqlite.JDBC'
 
     def build_jdbc_url(self):
-        self.db_path = os.path.abspath(self.connection_params['db_path'])
-        return f'jdbc:sqlite:{self.db_path}'
+        db_path = self.connection.extra.get('db_path', self.database or 'data.db')
+        db_path = os.path.abspath(db_path)
+        return f'jdbc:sqlite:{db_path}'
